4f38342ee003cc1a4403f0f80c0dea7e65e6e63f
remydegrave <bolieve.fr@gmail.com>
2020-11-09 15:51:57 +0100

    exercice4pt2Jq

---
+++ assets/js/fichierscript.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    // 
+    $('#red').click(function(){
+        $('#text').css('color','#b71f1f')
+    });
+
+    $('#blue').click(function(){
+        $('#text').css('color','#5969e0')
+    });
+
+});
